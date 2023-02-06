ace4ad19d4a45b0c9b916306f0a2291898bcb751
gamil <gamilbakirov@gmail.com>
2023-02-07 02:30:27 +0300

    fix get products

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -3,7 +3,7 @@ import data from '../../src/assets/json/products.json';
 // eslint-disable-next-line no-undef
 exports.handler = async () => {
   try {
-    return { statusCode: 200, body: JSON.stringify({ data }) };
+    return { statusCode: 200, body: JSON.stringify(data.data) };
   } catch (error) {
     console.log(error);
     return {
